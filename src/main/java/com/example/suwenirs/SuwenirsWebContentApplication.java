4b57ac4a5a6fa9e1f2ccf1ea7921269417b0b8d9
ArtyrV <voronaartur3@gmail.com>
2020-04-07 16:06:23 +0300

    Initial commit

---
+++ src/main/java/com/example/suwenirs/SuwenirsWebContentApplication.java
@@ -0,0 +1,12 @@
+package com.example.suwenirs;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuwenirsWebContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuwenirsWebContentApplication.class, args);
+    }
+}
